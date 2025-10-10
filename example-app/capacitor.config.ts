dce85a51f2cccdbc1b7187e516d1cbab398613f1
Martin Donadieu <martindonadieu@gmail.com>
2025-10-10 04:53:41 +0100

    chore: rename app

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'example-app',
+  appId: 'app.capgo.streamcall',
+  appName: 'Stream Call Example',
   webDir: 'www',
   "plugins": {
     "Keyboard": {
